d8bab676331d1517a786296a3f3e072e03616c26
aleks-goit <60502821+aleks-goit@users.noreply.github.com>
2021-02-03 02:29:52 +0200

    Updates

---
+++ precache-manifest.d747e558478a1eec8be50fb0b9412b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32effa568b9d2cd5c03aed2cc4ee35c6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c0693c47b4b599340eb2",
+    "url": "/goit-react-hw-01-components/static/css/main.898129b2.chunk.css"
+  },
+  {
+    "revision": "2bcc9b4ecfab798c0aea",
+    "url": "/goit-react-hw-01-components/static/js/2.a61df899.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.a61df899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0693c47b4b599340eb2",
+    "url": "/goit-react-hw-01-components/static/js/main.5e3d61cf.chunk.js"
+  },
+  {
+    "revision": "423d3956ea8fc8a28736",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.c56f1e61.js"
+  }
+]);
